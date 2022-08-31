e3b5153df7abebba883ead2607ed4cfb01e29644
Hamburger Réka <Rka>
2022-08-31 08:31:50 +0200

    Js3-001

--- 08-other/exercises/01/solution/js/removeDuplicatesFromArray.js
+++ 08-other/exercises/01/solution/js/removeDuplicatesFromArray.js
@@ -1,1 +1,33 @@
+
+/* Írj egy függvény kifejezést `removeDuplicatesFromArray` néven, 
+ami a paraméterként megadott tömbből eltávolítja a duplikált elemeket! 
+A tömb csak primítív értékeket tartalmaz! 
+A függvény visszatérési értéke a duplikált elemektől mentes új tömb!
+
+*/
+
+const removeDuplicatesFromArray=(arr=[])=>{
+    const spaceArr=[];
+    for(let i=0; i<arr.length; i++){
+        if (spaceArr.includes(arr[i])){
+           continue; 
+        }else{
+            spaceArr.push(arr[i]);
+        }
+      
+    } 
+    return spaceArr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 export default removeDuplicatesFromArray;
--- 08-other/exercises/03/solution/js/objectConverter.js
+++ 08-other/exercises/03/solution/js/objectConverter.js
@@ -1,1 +1,54 @@
+const objectConverter {
+    
+    arrayToMap(array){
+      const newMap=  new Map(); 
+      array.forEach(item=>{newMap.set(object.key,object.value)})
+      return newMap;
+    };
+    arrayToSet(array){
+        const newSet=  new Set(array); 
+      
+      return newSet;
+        
+    };
+    setToMap(set){
+         const newToMap=  new Map(); 
+      set.forEach(item=>{newToMap.set(object.key,object.value)})
+      return newToMap;
+        
+        
+    };
+    setToArray(set){
+        const arr=[...set];
+        return arr;
+        
+    };
+    mapToArray(map){
+        const arr=Array.from(map.values());
+        return arr;
+        
+    };
+    mapToSet(map){
+         const newSet=  new Set(map); 
+      
+      return newSet;
+        
+    };
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
 export default objectConverter;
